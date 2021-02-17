93c1eb9d0deb5e78a2337f4c87acb81bfd9f68a6
yangxingyao <yangxingyao02@bianfeng.com>
2021-02-17 23:13:10 +0800

    init

--- Assets/Scripts/BirdJump.cs
+++ Assets/Scripts/BirdJump.cs
@@ -8,7 +8,7 @@ public class BirdJump : MonoBehaviour{
 	// Use this for initialization
 	void Start (){
 		rigidbody2D = GetComponent<Rigidbody2D>();
-		//git test
+		//git test 222
 	}
 
 	public void AddForce(Vector2 force){
